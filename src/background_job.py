956fe14dfad27adf79d5b22a5adce5f4a9d096ba
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-05-14 14:12:33 +0200

    logging

---
+++ src/background_job.py
@@ -0,0 +1,9 @@
+# import logging
+
+
+# logger = logging.getLogger(__name__) # Module name, in this case 'background_job'
+# logger = logging.getLogger('MyTestLogger') # Custom name
+
+
+# def test_job():
+#     logger.info('Hello from test job')
