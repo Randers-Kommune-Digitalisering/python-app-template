4511ddffb8bf303179f8b44ff10e7608808bf4d0
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-05-22 15:03:55 +0200

    filter away calls to healthz and metrics

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,6 +1,8 @@
 import sys
 import logging
+import re
 
+from werkzeug import serving
 from prometheus_client import Gauge
 
 from utils.config import DEBUG
@@ -13,3 +15,14 @@ APP_RUNNING = Gauge('up', '1 - app is running, 0 - app is down', labelnames=['na
 def set_logging_configuration():
     log_level = logging.DEBUG if DEBUG else logging.INFO
     logging.basicConfig(stream=sys.stdout, level=log_level, format='[%(asctime)s] %(levelname)s - %(name)s - %(module)s:%(funcName)s - %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+    disable_endpoint_logs(('/metrics', '/healthz'))
+
+
+def disable_endpoint_logs(disabled_endpoints):
+    parent_log_request = serving.WSGIRequestHandler.log_request
+
+    def log_request(self, *args, **kwargs):
+        if not any(re.match(f"{de}$", self.path) for de in disabled_endpoints):
+            parent_log_request(self, *args, **kwargs)
+
+    serving.WSGIRequestHandler.log_request = log_request
