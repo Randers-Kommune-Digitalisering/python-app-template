e626b0217d1087dec4a93de29c320803df0026f3
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-06-20 16:19:23 +0200

    unit tests and corrections

--- src/utils/database.py
+++ src/utils/database.py
@@ -1,8 +1,6 @@
 import sqlalchemy
 import logging
 
-logger = logging.getLogger(__name__)
-
 
 class DatabaseClient:
     def __init__(self, db_type, database, username, password, host, port=None):
@@ -15,14 +13,24 @@ class DatabaseClient:
         else:
             raise ValueError(f"Invalid database type {type}")
 
+        self.logger = logging.getLogger(__name__)
+
         if port:
-            host = host + ':' + port
+            host = host + f':{port}'
 
         self.engine = sqlalchemy.create_engine(f'{driver}://{username}:{password}@{host}/{database}')
 
     def get_connection(self):
-        return self.engine.connect()
+        try:
+            if self.engine:
+                return self.engine.connect()
+            self.logger.error("DatabaseClient not initialized properly. Engine is None. Check error from init.")
+        except Exception as e:
+            self.logger.error(f"Error connecting to database: {e}")
 
     def execute_sql(self, sql):
-        with self.engine.connect() as conn:
-            return conn.execute(sqlalchemy.text(sql))
+        try:
+            with self.get_connection() as conn:
+                return conn.execute(sqlalchemy.text(sql))
+        except Exception as e:
+            self.logger.error(f"Error executing SQL: {e}")
